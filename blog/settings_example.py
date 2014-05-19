6e096fc10c7eb580ec11fbee585dd2aa3210e2b3
Colby Palmer <colby@colbypalmer.com>
2014-05-18 22:19:17 -0700

    add settings example

---
+++ blog/settings_example.py
@@ -0,0 +1,15 @@
+SITE_URL = "http://project.com"
+SITE_NAME = "Project Name"
+
+COMMENTS_APP = 'threadedcomments' # for example
+RECAPTCHA_PUBLIC_KEY = 'put-your-key-here'
+RECAPTCHA_PRIVATE_KEY = 'put-your-key-here'
+
+SOUTH_MIGRATION_MODULES = {
+    'taggit': 'taggit.south_migrations',
+}
+
+TAGGIT_TAGCLOUD_MIN = 1
+TAGGIT_TAGCLOUD_MAX = 8
+
+GRAPPELLI_ADMIN_TITLE = u'{} Administration'.format(SITE_NAME)
