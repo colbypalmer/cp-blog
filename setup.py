17e141192e5dfec81bab1330990ebd3ba9ecf1e2
Colby Palmer <colby@colbypalmer.com>
2014-05-30 00:16:02 -0700

    new package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-cp-blog',
     version='0.1.1',
-    packages=['blog'],
+    packages=['blog',],
     include_package_data=True,
     license='MIT License',  # example license
     description='A basic blog app for Django.',
