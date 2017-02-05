9cbbd55ed1c25cf0ed92d9e03d9cc11142f1bffa
Colby Palmer <colbypalmer@users.noreply.github.com>
2017-02-05 13:32:19 -0800

    Update urls for django 1.10.5

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,16 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import url
 from blog.feeds import LatestEntriesRssFeed, LatestEntriesAtomFeed
 
-admin.autodiscover()
-
-urlpatterns = patterns('',
-                       url(r'^$', 'blog.views.blog_list', name='blog'),
-                       url(r'^rss/$', LatestEntriesRssFeed(), name='blog_rss'),
-                       url(r'^atom/$', LatestEntriesAtomFeed(), name='blog_atom'),
-                       url(r'^tags/$', 'blog.views.blog_tags', name="blog_tags"),
-                       url(r'^search/$', 'blog.views.blog_search', name='blog_search'),
-                       url(r'^archive/date/$', 'blog.views.blog_archive_date', name="blog_archive_date"),
-                       url(r'^archive/title/$', 'blog.views.blog_archive_title', name="blog_archive_title"),
-                       url(r'^month/(\d+)/(\d+)/$', 'blog.views.month', name="blog_month"),
-                       url(r'^(?P<slug>[\w\._-]+)/$', 'blog.views.blog_detail', name='blog_detail'),
-                       url(r'^category/(?P<slug>[\w\._-]+)/$', 'blog.views.blog_entries_by_category',
-                           name='blog_entries_by_category'),
-                       url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/$', 'blog.views.blog_entries_by_tag',
-                           name="blog_entries_by_tag"),
-                       url(r'^grappelli/', include('grappelli.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       )
+urlpatterns = [
+    url(r'^$', 'blog.views.blog_list', name='blog'),
+    url(r'^rss/$', LatestEntriesRssFeed(), name='blog_rss'),
+    url(r'^atom/$', LatestEntriesAtomFeed(), name='blog_atom'),
+    url(r'^tags/$', 'blog.views.blog_tags', name='blog_tags'),
+    url(r'^search/$', 'blog.views.blog_search', name='blog_search'),
+    url(r'^archive/date/$', 'blog.views.blog_archive_date', name='blog_archive_date'),
+    url(r'^archive/title/$', 'blog.views.blog_archive_title', name='blog_archive_title'),
+    url(r'^month/(\d+)/(\d+)/$', 'blog.views.month', name='blog_month'),
+    url(r'^(?P<slug>[\w\._-]+)/$', 'blog.views.blog_detail', name='blog_detail'),
+    url(r'^category/(?P<slug>[\w\._-]+)/$', 'blog.views.blog_entries_by_category', name='blog_entries_by_category'),
+    url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/$', 'blog.views.blog_entries_by_tag', name='blog_entries_by_tag'),
+]
