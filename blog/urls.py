c00e2a5b58f0e0d604772bef6d85c4f86d2d3a43
Colby Palmer <colbypalmer@users.noreply.github.com>
2017-02-05 16:33:22 -0800

    update urls with CBVs

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
-from blog.feeds import LatestEntriesRssFeed, LatestEntriesAtomFeed
+from .feeds import LatestEntriesRssFeed, LatestEntriesAtomFeed
+from .views import BlogListView, BlogDetailView, BlogTagsView, BlogSearchView, BlogArchiveDateView, BlogArchiveTitleView, \
+    BlogEntriesByCategoryView, BlogEntriesByTagView, BlogMonthView
 
 urlpatterns = [
-    url(r'^$', 'blog.views.blog_list', name='blog'),
+    url(r'^$', BlogListView.as_view(), name='blog'),
     url(r'^rss/$', LatestEntriesRssFeed(), name='blog_rss'),
     url(r'^atom/$', LatestEntriesAtomFeed(), name='blog_atom'),
-    url(r'^tags/$', 'blog.views.blog_tags', name='blog_tags'),
-    url(r'^search/$', 'blog.views.blog_search', name='blog_search'),
-    url(r'^archive/date/$', 'blog.views.blog_archive_date', name='blog_archive_date'),
-    url(r'^archive/title/$', 'blog.views.blog_archive_title', name='blog_archive_title'),
-    url(r'^month/(\d+)/(\d+)/$', 'blog.views.month', name='blog_month'),
-    url(r'^(?P<slug>[\w\._-]+)/$', 'blog.views.blog_detail', name='blog_detail'),
-    url(r'^category/(?P<slug>[\w\._-]+)/$', 'blog.views.blog_entries_by_category', name='blog_entries_by_category'),
-    url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/$', 'blog.views.blog_entries_by_tag', name='blog_entries_by_tag'),
+    url(r'^tags/$', BlogTagsView.as_view(), name='blog_tags'),
+    url(r'^search/$', BlogSearchView.as_view(), name='blog_search'),
+    url(r'^archive/date/$', BlogArchiveDateView.as_view(), name='blog_archive_date'),
+    url(r'^archive/title/$', BlogArchiveTitleView.as_view(), name='blog_archive_title'),
+    url(r'^month/(\d+)/(\d+)/$', BlogMonthView.as_view(), name='blog_month'),
+    url(r'^(?P<slug>[\w\._-]+)/$', BlogDetailView.as_view(), name='blog_detail'),
+    url(r'^category/(?P<slug>[\w\._-]+)/$', BlogEntriesByCategoryView.as_view(), name='blog_entries_by_category'),
+    url(r'^tag/(?P<tag>[-_A-Za-z0-9]+)/$', BlogEntriesByTagView.as_view(), name='blog_entries_by_tag'),
 ]
